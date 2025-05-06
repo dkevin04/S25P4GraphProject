85e2c6e08000de806072c407ca820e172a827aa6
dkevin04 <148819379+dkevin04@users.noreply.github.com>
2025-05-05 20:56:24 -0400

    Finished hash and fully tested

--- src/Hash.java
+++ src/Hash.java
@@ -2,101 +2,181 @@
 /**
  * Hash table class
  *
- * @author <Blake Everman> 
+ * @author <Blake Everman>
  * @version <5.1.25>
  */
 
 public class Hash<K extends Comparable<K>, E> {
 
-	private KVPair<K, E>[] table;
+    private KVPair<K, E>[] table;
+    private KVPair<K, E> tombstone;
 
-	private int capacity;
+    private int capacity;
 
-	private int size;
+    private int size;
 
-	/**
-	 * Compute the hash function
-	 * 
-	 * @param s      The string that we are hashing
-	 * @param length Length of the hash table (needed because this method is static)
-	 * @return The hash function value (the home slot in the table for this key)
-	 */
-	public static int h(String s, int length) {
-		int intLength = s.length() / 4;
-		long sum = 0;
-		for (int j = 0; j < intLength; j++) {
-			char[] c = s.substring(j * 4, (j * 4) + 4).toCharArray();
-			long mult = 1;
-			for (int k = 0; k < c.length; k++) {
-				sum += c[k] * mult;
-				mult *= 256;
-			}
-		}
+    /**
+     * Compute the hash function
+     * 
+     * @param s
+     *            The string that we are hashing
+     * @param length
+     *            Length of the hash table (needed because this method is
+     *            static)
+     * @return The hash function value (the home slot in the table for this key)
+     */
+    public static int h(String s, int length) {
+        int intLength = s.length() / 4;
+        long sum = 0;
+        for (int j = 0; j < intLength; j++) {
+            char[] c = s.substring(j * 4, (j * 4) + 4).toCharArray();
+            long mult = 1;
+            for (int k = 0; k < c.length; k++) {
+                sum += c[k] * mult;
+                mult *= 256;
+            }
+        }
 
-		char[] c = s.substring(intLength * 4).toCharArray();
-		long mult = 1;
-		for (int k = 0; k < c.length; k++) {
-			sum += c[k] * mult;
-			mult *= 256;
-		}
+        char[] c = s.substring(intLength * 4).toCharArray();
+        long mult = 1;
+        for (int k = 0; k < c.length; k++) {
+            sum += c[k] * mult;
+            mult *= 256;
+        }
 
-		return (int) (Math.abs(sum) % length);
-	}
+        return (int)(Math.abs(sum) % length);
+    }
 
-	public String find(K key) {
-		int home;
-		int pos = home = h((String) key, capacity);
-		for (int i = 1; table[pos] != null && (!key.equals(table[pos].key())) && i <= capacity; i++) {
-			pos = (home + i*i) % capacity;
-		}
-		if (table[pos] != null && key.equals(table[pos].key())) {
-			return (String) table[pos].value();
-		}
-		else {
-			return null;
-		}
 
-	}
+    @SuppressWarnings("unchecked")
+    public Hash(int hashSize) {
+        table = new KVPair[hashSize];
+        capacity = hashSize;
+        size = 0;
+        Graph element = new Graph();
+        tombstone = new KVPair<K, E>((K)"TOMBSTONE", (E)element);
+    }
 
-	public int prints() {
-		int count = 0;
-		/*
-		 * prints all the hash tale values and returns count
-		 */
-		return count;
-	}
 
-	public void expandTable() {
-		
-	}
+    @SuppressWarnings("unchecked")
+    public Hash() {
+        table = new KVPair[1000];
+        capacity = 1000;
+        size = 0;
+        E element = null;
+        tombstone = new KVPair<K, E>((K)"TOMBSTONE", element);
+    }
 
-	public boolean insert(K key, E element) {
-		/*
-		 * if expandsion needed...
-		 */
-		if (size +1 / capacity > 0.5) {
-			expandTable();
-		}
-		/*
-		 * if not then proceed to insert
-		 */
-		int home;
-		int pos = home = h((String) key, capacity);
-		for (int i = 1; table[pos] != null && (!key.equals(table[pos].key())) && i <= capacity; i++) {
-			pos = (home + i*i) % capacity;
-		}
-		if (table[pos] != null && key.equals(table[pos].key())) {
-			table[pos] = new KVPair<>(key, element);
-			size++;
-			return true;
-		}
-		return false;
-	}
 
-	public void remove(String key) {
-		/*
-		 * removes using tombstone
-		 */
-	}
+    public String find(K key) {
+        int home;
+        int pos = home = h((String)key, capacity);
+        for (int i = 1; table[pos] != null && (!key.equals(table[pos].key()))
+            && i <= capacity; i++) {
+            pos = (home + i * i) % capacity;
+        }
+        if (table[pos] != null && key.equals(table[pos].key())) {
+            return table[pos].value().toString();
+        }
+        else {
+            return null;
+        }
+    }
+
+
+    public int search(K key) {
+        int home;
+        int pos = home = h((String)key, capacity);
+        for (int i = 1; table[pos] != null && (!key.equals(table[pos].key()))
+            && i <= capacity; i++) {
+            pos = (home + i * i) % capacity;
+        }
+        if (table[pos] != null && key.equals(table[pos].key())) {
+            return pos;
+        }
+        else {
+            return -1;
+        }
+    }
+
+
+    public int prints() {
+        int count = 0;
+        /*
+         * prints all the hash tale values and returns count
+         */
+        int index = 0;
+        while (index < table.length) {
+            if (table[index] != null) {
+                System.out.println(table[index]);
+                count++;
+            }
+            index++;
+        }
+        return count;
+    }
+
+
+    public void expandTable() {
+        @SuppressWarnings("unchecked")
+        KVPair<K, E>[] newTable = new KVPair[capacity * 2];
+        int index = 0;
+        while (index < newTable.length) {
+            if (newTable[index] != null) {
+                int newHome = h(newTable[index].key().toString(), table.length);
+                newTable[newHome] = table[index];
+            }
+            index++;
+        }
+        table = newTable;
+        capacity = capacity * 2;
+    }
+
+
+    public boolean insert(K key, E element) {
+        /*
+         * if expansion needed...
+         */
+        if (size + 1 > (capacity / 2)) {
+            expandTable();
+        }
+        /*
+         * if not then proceed to insert
+         */
+        int home;
+        int pos = home = h((String)key, capacity);
+        for (int i = 1; table[pos] != null && i <= capacity; i++) {
+            pos = (home + i * i) % capacity;
+        }
+        if (table[pos] == null) {
+            table[pos] = new KVPair<>(key, element);
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+
+    public void remove(K key) {
+        /*
+         * removes using tombstone
+         */
+        int index = search(key);
+        if (index >= 0) {
+            table[index] = tombstone;
+            size--;
+            return;
+        }
+    }
+
+
+    public int getSize() {
+        return size;
+    }
+
+
+    public KVPair<K, E>[] getTable() {
+        return table;
+    }
 
 }
--- src/HashTest.java
+++ src/HashTest.java
@@ -5,11 +5,20 @@ import student.TestCase;
  * @version <Put something here>
  */
 public class HashTest extends TestCase {
+    private Hash hash;
+    private Hash smallHash;
+    private Hash emptyHash;
+    private Graph graph;
+
     /**
      * Sets up the tests that follow. In general, used for initialization
      */
+    @SuppressWarnings("rawtypes")
     public void setUp() {
-        // Nothing Here
+        hash = new Hash(10000);
+        smallHash = new Hash(10);
+        emptyHash = new Hash(100);
+        graph = new Graph();
     }
 
 
@@ -29,5 +38,85 @@ public class HashTest extends TestCase {
         assertTrue(Hash.h("Long Lonesome Blues", 10000) == 4635);
         assertTrue(Hash.h("Long   Lonesome Blues", 10000) == 4159);
         assertTrue(Hash.h("long Lonesome Blues", 10000) == 4667);
+        assertTrue(Hash.h("q'", 10000) == 97);
+    }
+
+
+    /**
+     * Tests the insert method and by extension the search method
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public void testInsert() throws Exception {
+        assertEquals(hash.getSize(), 0);
+        hash.insert("a", graph);
+        assertEquals(hash.search("a"), 97);
+        assertEquals(hash.getSize(), 1);
+        hash.insert("b", graph);
+        assertEquals(hash.search("b"), 98);
+        hash.insert("aaaa", graph);
+        assertEquals(hash.search("aaaa"), 1873);
+        hash.insert("aaab", graph);
+        assertEquals(hash.search("aaab"), 9089);
+        assertEquals(hash.getSize(), 4);
+        hash.insert("baaa", graph);
+        assertEquals(hash.search("baaa"), 1874);
+        hash.insert("Long Lonesome Blues", graph);
+        assertEquals(hash.search("Long Lonesome Blues"), 4635);
+        hash.insert("Long   Lonesome Blues", graph);
+        assertEquals(hash.search("Long   Lonesome Blues"), 4159);
+        hash.insert("long Lonesome Blues", graph);
+        assertEquals(hash.search("long Lonesome Blues"), 4667);
+        assertEquals(hash.getSize(), 8);
+        hash.insert("a", graph);
+        assertTrue(hash.getTable()[101].toString().equals("(a, )"));
+        hash.insert("a", graph);
+        assertTrue(hash.getTable()[106].toString().equals("(a, )"));
+        assertEquals(hash.getSize(), 10);
+        hash.insert("q'", graph);
+        assertEquals(hash.search("q'"), 113);
+        assertEquals(hash.getSize(), 11);
+        assertEquals(hash.search("Beatles"), -1);
+        assertEquals(smallHash.getTable().length, 10);
+        smallHash.insert("a", graph);
+        smallHash.insert("b", graph);
+        smallHash.insert("c", graph);
+        smallHash.insert("d", graph);
+        smallHash.insert("e", graph);
+        smallHash.insert("f", graph);
+        smallHash.prints();
+        assertEquals(smallHash.getTable().length, 20);
+        emptyHash.prints();
+        smallHash = new Hash(10);
+        smallHash.insert("c", graph);
+        smallHash.insert("c", graph);
+        smallHash.insert("c", graph);
+    }
+
+
+    /**
+     * Tests the remove method
+     */
+    @SuppressWarnings("unchecked")
+    public void testRemove() throws Exception {
+        hash.insert("a", graph);
+        assertTrue(hash.getTable()[97].toString().equals("(a, )"));
+        hash.insert("b", graph);
+        assertEquals(hash.search("a"), 97);
+        assertEquals(hash.search("b"), 98);
+        assertFalse(hash.getTable()[98].toString().equals("(TOMBSTONE, )"));
+        hash.remove("b");
+        assertEquals(hash.search("b"), -1);
+        assertEquals(hash.getSize(), 1);
+        assertTrue(hash.getTable()[98].toString().equals("(TOMBSTONE, )"));
+        System.out.println(hash.getTable()[98].toString());
+        assertTrue(hash.getTable()[98].toString().equals("(TOMBSTONE, )"));
+        assertFalse(hash.getTable()[97].toString().equals("(TOMBSTONE, )"));
+        hash.remove("Queen");
+        assertEquals(hash.search("Queen"), -1);
+        smallHash.insert("a", graph);
+        smallHash.insert("b", graph);
+        smallHash.insert("c", graph);
+        smallHash.remove("c");
+        assertEquals(smallHash.getSize(), 2);
     }
 }
